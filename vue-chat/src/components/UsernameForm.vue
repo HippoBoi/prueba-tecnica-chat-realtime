7fa1451e1fd4f7ea9d1440dacc0d2a83624f1656
Andy <tagrifavc@gmail.com>
2026-03-12 11:22:04 -0300

    add current username visual element

--- vue-chat/src/components/UsernameForm.vue
+++ vue-chat/src/components/UsernameForm.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useChatStore } from '../stores/chatStore';
 
+const username = ref<string>('');
 const chatStore = useChatStore();
-const input = ref('');
+const input = ref<string>('');
 
 function handleSubmit() {
   const trimmed = input.value.trim();
   if (!trimmed) return;
   chatStore.setUsername(trimmed);
+  username.value = trimmed;
   input.value = '';
 }
+
+onMounted(() => {
+  username.value = chatStore.username;
+})
+
 </script>
 
 <template>
-  <div>
-    <h2>Change Username</h2>
+  <div className="form-container">
+    <h3>Username: {{ username }}</h3>
+    <p>Change Username</p>
     <input
       v-model="input"
       type="text"
@@ -25,3 +33,25 @@ function handleSubmit() {
     <button :disabled="!input.trim()" @click="handleSubmit">Change</button>
   </div>
 </template>
+
+<style>
+  .form-container {
+    position: fixed;
+    top: 0;
+    left: 1%;
+  }
+
+  .input-container p {
+    justify-self: center;
+    margin-bottom: 0.25rem;
+  }
+
+  .input-container h3 {
+    justify-self: center;
+    margin-bottom: 0.25rem;
+  }
+
+  .input-container input {
+    margin-bottom: 2rem;
+  }
+</style>
