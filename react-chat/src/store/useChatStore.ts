d35beb88ff3cc6f5ec249be362fc99c4b1908b23
Andy <tagrifavc@gmail.com>
2026-06-15 22:37:31 -0400

    remove saving in local storage

--- react-chat/src/store/useChatStore.ts
+++ react-chat/src/store/useChatStore.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 import type { Message } from '../types/message';
 
 interface ChatState {
@@ -15,30 +14,18 @@ interface ChatState {
   setProfilePictureIndex: (index: number) => void;
 }
 
-export const useChatStore = create<ChatState>()(
-  persist(
-    (set) => ({
-      messages: [],
-      isConnected: false,
-      isChatVisible: false,
-      username: '',
-      profilePictureIndex: 0,
-      addMessage: (message) =>
-        set((state) => ({
-          messages: [...state.messages, message],
-        })),
-      setConnected: (connected) => set({ isConnected: connected }),
-      setChatVisible: (visible) => set({ isChatVisible: visible }),
-      setUsername: (username) => set({ username }),
-      setProfilePictureIndex: (index) => set({ profilePictureIndex: index }),
-    }),
-    {
-      name: 'chat-storage',
-      partialize: (state) => ({
-        messages: state.messages,
-        username: state.username,
-        profilePictureIndex: state.profilePictureIndex,
-      }),
-    }
-  )
-);
+export const useChatStore = create<ChatState>()((set) => ({
+  messages: [],
+  isConnected: false,
+  isChatVisible: false,
+  username: '',
+  profilePictureIndex: 0,
+  addMessage: (message) =>
+    set((state) => ({
+      messages: [...state.messages, message],
+    })),
+  setConnected: (connected) => set({ isConnected: connected }),
+  setChatVisible: (visible) => set({ isChatVisible: visible }),
+  setUsername: (username) => set({ username }),
+  setProfilePictureIndex: (index) => set({ profilePictureIndex: index }),
+}));
